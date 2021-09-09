738748a30ba6b3003a2dc120b4624fe61ea43dd6
= <salaslilia00@gmail.com>
2021-09-08 21:29:04 -0400

    Design shell for FAQ's Page Version 2

---
+++ src/pages/Gallery.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+
+export default function Gallery(props) {
+	return <div className="Gallery"></div>;
+}
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,7 @@ import About from '../pages/About';
 import Home from '../pages/Home';
 import Contact from '../pages/Contact';
 import Faqs from '../pages/Faqs';
+import Gallery from '../pages/Gallery';
 
 const routes = [
 	{
@@ -30,6 +31,11 @@ const routes = [
 		Component: Faqs,
 		key: 'Faqs',
 		path: '/Faqs'
+	},
+	{
+		Component: Gallery,
+		key: 'Gallery',
+		path: '/Gallery'
 	}
 ];
 
