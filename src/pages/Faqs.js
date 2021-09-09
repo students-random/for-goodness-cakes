e912310c1b7433de566f33612a3df45f01a325f6
= <salaslilia00@gmail.com>
2021-09-08 20:06:24 -0400

    Design shell for FAQ's Page

---
+++ src/pages/Faqs.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+export default function Home(props) {
+	return <div className="HomePage">
+<h1>Frequently Asked Questions</h1>
+<ul>
+	<li>
+	<span>So what goes into a custom cake?</span>
+	<p>The world of custom cake pricing is much different than a traditional style birthday cake. The possibilities are far greater, and the memories that come with beautiful customized cakes far outlive the cake itself! Here are some FAQ's for ordering and pricing for custom cakes.</p>
+	</li>
+	<li>
+  <span>What is a "custom" cake?</span>
+	 <p>A custom cake is any type of cake that goes above and beyond the call of regular cake work. This can include handmade fondant pieces, flowers, or attention to detail that is a little bit extra than a traditional style birthday cake. Tiered cakes and wedding cakes are also considered custom. Adding deliveries to orders also is dependent on availability, so we also consider deliveries "custom".</p></li>
+
+	<li><span>How much do custom cakes cost?</span>
+	<p>All of our custom cakes are priced out on an individual basis, so we will work with you to figure out exactly what will work for your guest count and budget. Generally, the cost of a custom cake starts with the size, cake flavors, and design work. From there, labor hours are factored in as well. Please visit our page Cake Pricing for some basic information on cake pricing.</p></li>
+
+	<li><span>Why isn't the cake cost just the base price on the website?</span>
+	<p>The base pricing on our website lets you know how many servings you will get for each size, and the pricing for BASIC cake work (frosting with minimal decoration). Once we are adding hours of work to the cake, the price will of course be adjusted. Pricing for custom cake work is linked to time. If you let us know your budget upfront, we can let you know what is or isn't possible for your budget, and we can vary designs to work accordingly.</p></li>
+
+	<li><span>Why is there a certain time frame for placing a custom cake order?</span>
+	<p>Custom cake work takes time, and unfortunately there's only so much of that to go around! We ask for a MINIMUM of 10 to 14 DAYS notice for custom cake work, depending on the level of customization you're looking for. <span>14 days notice means that your order has been placed, priced, and your deposit has been received 10-14 days before your order is needed</span>. Failure to plan on your part, does not constitute an emergency on our part! If we are already booked up for a weekend, we will let you know. We often can, and do accommodate last minute requests, but it cannot be guaranteed if you are not within the proper time frame, especially during wedding season (April-October).</p></li>
+
+
+	<li><span>Why do I need to place a deposit for my custom order?</span>
+	<p>When we accept your custom cake order, there is a lot that goes into making sure your order gets completed on time. From ordering materials, to hand-making pieces, and even turning down other orders to ensure your order is completed to the highest standard. A deposit will let us know you're serious about placing your order, and allow us to get the ball rolling to completing your order. <span>Deposits are NON-REFUNDABLE, because the deposit will reserve your event date and labor hours. Deposits are 50% of the total of the custom order</span>. Meaning we turn down other orders, order materials ahead of time, etc, to make sure your order is completed to the highest standard. ORDERS ARE NOT CONFIRMED UNTIL THE INITIAL DEPOSIT IS PAID. </p></li>
+
+	<li><span>Can I taste cakes before ordering?</span>
+	<p>Cake consultations and tastings are available for weddings only. We typically book cake tastings during the week, as weekends can be a bit busy around the shop. However, if your schedule only allows weekend availability, we will make every effort to find time to meet with you based on the availability in our schedule.</p></li>
+
+	<li><span>Do you deliver?</span>
+	<p>Yes! We strongly encourage large cakes and specialty cakes be delivered to your event location to ensure there's no issues with transportation. We figure out the delivery cost for you once you provide your location and time.
+	<span>Delivery available, based on availability for orders over $100.</span>
+	If you want to pick the cake up yourself, we have you sign your order form which states that you assume all responsibility for your cake once it leaves our shop. We cannot be responsible for cakes once they leave our shop, as we do offer a delivery service which guarantees a perfect cake to arrive at its location.</p></li>
+
+	<li><span>I sent an email for a custom cake, when will I hear back?</span>
+	<p>Emailing orders can sometimes be a slower way to place an order, as we DO NOT respond to emails on the weekends. We are a small business with a limited amount of bakers and one cake decorator, so on the weekends it is next to impossible to get to emails on top of the custom cake work we do. <span>PLEASE DO NOT SEND MULTIPLE EMAILS. It will take us longer to respond to you, because our emails are answered in the order they are received, and the more emails you send without a response, the further down it bumps you on our end.</span> We try to focus all of our attention to our custom work, so just remember, when it takes a slower amount of time to hear back on the weekend, we are dedicating ourselves to our work, just like we will when we are making your order! </p></li>
+
+
+</ul>
+
+	</div>;
+}
