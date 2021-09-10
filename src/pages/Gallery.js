f73502b02a5717b1fe6a46e1c13ce5bf95ce9b6b
= <salaslilia00@gmail.com>
2021-09-09 22:28:14 -0400

    Design shell for Gallery Page

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,7 +1,66 @@
+import React, { useState } from 'react';
+
 export default function Gallery(props) {
 	return (
 		<div className="Gallery">
 			<h1>Gallery</h1>
+			<div>
+				<div>
+					<img src="/img/pineapple-cake.png" />
+				</div>
+				<div>
+					<img src="/img/wedding-cake.png" />
+				</div>
+				<div>
+					<img src="/img/ares-cake.png" />
+				</div>
+				<div>
+					<img src="/img/blue-white-cake-pops.png" />
+				</div>
+				<div>
+					<img src="/img/happy-50th-cake.png" />
+				</div>
+				<div>
+					<img src="/img/cowboy-hat-cake.png" />
+				</div>
+				<div>
+					<img src="/img/cupcakes-00.png" />
+				</div>
+				<div>
+					<img src="/img/cupcakes-03.png" />
+				</div>
+				<div>
+					<img src="/img/god-bless-madison-cake.png" />
+				</div>
+				<div>
+					<img src="/img/happy-birthday-anna-cake.png" />
+				</div>
+				<div>
+					<img src="/img/happy-birthday-cake-03.png" />
+				</div>
+				<div>
+					<img src="/img/happy-birthday-cake-04.png" />
+				</div>
+				<div>
+					<img src="/img/happy-birthday-cake-05.png" />
+				</div>
+				<div>
+					<img src="/img/happy-birthday-cake-pops-01.png" />
+				</div>
+				<div>
+					<img src="/img/ice-cream-cone-cakes.png" />
+				</div>
+				<div>
+					<img src="/img/love-you-mom-cake.png" />
+				</div>
+
+				<div>
+					<img src="/img/purple-doughnuts.png" />
+				</div>
+				<div>
+					<img src="/img/winnie-the-pooh-cupcakes.png" />
+				</div>
+			</div>
 		</div>
 	);
 }
