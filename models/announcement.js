7cf59263b983de83ccea04a5717918fbe5df0acf
Yuliap21 <yulia777best@gmail.com>
2021-09-11 14:41:43 -0400

    announcement schema updated and cake.js created

--- models/announcement.js
+++ models/announcement.js
@@ -1,10 +1,10 @@
 const { Schema, model } = require ('mongoose');
 
 const announcementSchema = new Schema({
-name: { type: String, required: true, unique: true },
-body: { type: String, required: true, unique: true }
+name: { type: String, required: true },
+body: { type: String, required: true }
 },{
   timestamps: true
-}) 
+})
 
 module.exports = model('Announcement', announcementSchema);
