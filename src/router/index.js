f7ea941a62f4267830340412c6a6ef6a960c9046
Cat <morancathy@gmail.com>
2021-09-14 15:31:51 -0500

    debugged import of footer

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import Footer from '../components/NavBar';
+import Footer from '../components/Footer';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import routes from './routes';
 const AppRouter = () => {
