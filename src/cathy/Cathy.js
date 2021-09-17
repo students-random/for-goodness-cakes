7c1a2659eb838cb3539da877e295d61e79c83f0a
Cat <morancathy@gmail.com>
2021-09-16 23:28:28 -0500

    created Cathy notes folder

---
+++ src/cathy/Cathy.js
@@ -0,0 +1,20 @@
+// put in styles.scss
+@import "calendar";
+
+
+//index.js
+import OrderForm from '../components/OrderForm';
+
+<Route
+  path={'/orderform'}
+  render={routerProps => <OrderForm {...routerProps} />}
+></Route>
+
+
+// menu.js
+import { Link} from 'react-router-dom';
+import OrderForm from '../components/OrderForm';
+
+<div className="MenuPage">
+  <OrderForm />
+</div>
