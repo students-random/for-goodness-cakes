d1e8c73119a5efc4f748ad209d189d310fbba115
Cat <morancathy@gmail.com>
2021-09-23 03:13:19 -0500

    links in Admin page only show when you click/hover

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import LogIn from '../components/LogIn';
-// import Annoucements from '../components/Annoucements';
+import Annoucements from '../components/Annoucements';
 import Orders from '../components/Orders';
 import DisableDay from '../components/DisableDay';
 import ContactSubmissions from '../components/ContactSubmissions';
@@ -12,6 +12,29 @@ export default function Admin(props) {
 		password: ''
 	});
 	const [loggedInUser, setLoggedInUser] = useState('');
+	const [viewOrder, setViewOrder] = useState(false);
+	const [viewContact, setViewContact] = useState(false);
+	const [viewDisable, setViewDisable] = useState(false);
+	const [viewAnnouc, setViewAnnouc] = useState(false);
+
+	const toggle = setComponent => {
+		setViewOrder(false);
+		setViewContact(false);
+		setViewDisable(false);
+		setViewAnnouc(false);
+		setComponent(true);
+	};
+
+	const bold = component => {
+		if (component) {
+			return {
+				backgroundColor: '#D0BAC4',
+				color: 'white',
+				borderRadius: '500px',
+				boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'
+			};
+		}
+	};
 
 	useEffect(() => {
 		if (window.localStorage.getItem('token')) {
@@ -25,17 +48,12 @@ export default function Admin(props) {
 		window.location.assign('/admin');
 	};
 
-	const works = () => {
-		console.log('works');
-	};
-
 	return (
 		<div className="Admin">
 			{!token ? (
 				<>
 					{' '}
 					<LogIn
-						works={{ works }}
 						token={token}
 						setToken={setToken}
 						user={user}
@@ -48,19 +66,28 @@ export default function Admin(props) {
 				<>
 					{' '}
 					<div className="links">
-						<h1>Orders</h1>
-						<h1>'Contact Me' Submissions</h1>
-						<h1>Disable Dates</h1>
-						<h1>Annoucements</h1>
+						<button onClick={() => toggle(setViewOrder)}>
+							<h1 style={bold(viewOrder)}>Orders</h1>
+						</button>
+						<button onClick={() => toggle(setViewContact)}>
+							<h1 style={bold(viewContact)}>'Contact Me' Submissions</h1>
+						</button>
+						<button onClick={() => toggle(setViewDisable)}>
+							<h1 style={bold(viewDisable)}>Disable Date</h1>
+						</button>
+						<button onClick={() => toggle(setViewAnnouc)}>
+							<h1 style={bold(viewAnnouc)}>Annoucements</h1>
+						</button>
 
 						<button onClick={logout} className="">
 							{' '}
 							<h1>Log Out</h1>
 						</button>
 					</div>
-					<DisableDay />
-					<Orders />
-					<ContactSubmissions />
+					{viewOrder && <Orders />}
+					{viewContact && <ContactSubmissions />}
+					{viewDisable && <DisableDay />}
+					{viewAnnouc && <Annoucements />}
 				</>
 			)}
 		</div>
