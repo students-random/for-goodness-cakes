861925c58eb9c6a0c64a896fd06be9ad54fab241
en-coded <83879346+en-coded@users.noreply.github.com>
2021-09-28 12:45:44 -0400

    Menu Page

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -5,6 +5,24 @@ export default function Menu(props) {
 	return (
 		<div className="MenuPage">
 			<div>Main Menu Page</div>
+
+			<div className="gallery-container">
+				<a target="blank" href="https://www.instagram.com/p/CSn1fdYsKCw">
+					{' '}
+					<img
+						className="cake"
+						src="/img/Web-Occasion-Cakes.png"
+						onClick={() => imageClick()}
+					/>
+				</a>
+				<a target="blank" href="https://www.instagram.com/p/CPeb5FknLRf/">
+					<img className="cake" src="/img/Web-Cupcakes.png" />
+				</a>
+				<a target="blank" href="https://www.instagram.com/p/CRVnRRonMf9/">
+					<img className="cake" src="/img/Web-Other Sweets.png" />
+				</a>
+			</div>
+
 			<Link to={'/orderform'}>
 				<button className="btn-main">Request an Invoice</button>
 			</Link>
